37344a77df3e3d4a7bbbcb11e2fc37342dce9577
xjba <xjba@example.test>
2021-08-10 21:32:18 +0900

    generators用イニシャライザを追加

---
+++ config/initializers/generators.rb
@@ -0,0 +1,17 @@
+Rails.application.config.generators do |g|
+  # g.assets false
+  # g.skip_routes true
+  # g.stylesheets false
+  # g.scaffold_stylesheet false
+  # g.javascripts false
+  # g.helper false
+  # g.template_engine false
+  # g.test_framework false
+  # g.test_framework :rspec,
+  #   controller_specs: false,
+  #   view_specs: false,
+  #   helper_specs: false,
+  #   routing_specs: false,
+  #   fixture: true
+  # g.fixture_replacement :factory_bot, dir: 'spec/support/factories'
+end
