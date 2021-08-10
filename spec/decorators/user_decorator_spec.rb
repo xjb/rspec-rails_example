247c792377d746e8a63d3c7ee756f7095b475b9f
xjba <xjba@example.test>
2021-08-10 20:35:39 +0900

    UserDecoratorのテストを実装

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -3,7 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
-  let(:user) { User.new.extend UserDecorator }
+  let(:user) { build(:user).extend UserDecorator }
   subject { user }
   it { should be_a User }
+
+  context '#name_addr' do
+    subject { user.name_addr }
+    it { is_expected.to match /.+<.+>/ }
+    it { is_expected.to start_with user.name }
+    it { is_expected.to include user.email }
+  end
 end
